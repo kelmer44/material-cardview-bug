78f855acdd823de898ee12dbab46ad34ea1e929d
kelmer <kelmer@gmail.com>
2020-01-20 09:26:11 +0100

    demo app

---
+++ app/src/main/java/net/kelmer/android/materialcardview/MainActivity.kt
@@ -0,0 +1,30 @@
+package net.kelmer.android.materialcardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_cardview.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val behavior = BottomSheetBehavior.from(bottom_sheet).apply {
+            state = BottomSheetBehavior.STATE_HIDDEN
+        }
+        button_opensheet.setOnClickListener {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+
+        show_layer_button.setOnClickListener {
+            test_layer.isVisible = !test_layer.isVisible
+        }
+
+    }
+}
